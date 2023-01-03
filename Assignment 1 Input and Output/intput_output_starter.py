d45e0dff495aeab2e3f32824907931b3ee349311
Keith Bergreen <keith.bergreen@ivschools.org>
2023-01-03 08:42:26 -0500

    Create intput_output_starter.py
    Adding assignment 1

---
+++ Assignment 1 Input and Output/intput_output_starter.py
@@ -0,0 +1,7 @@
+name = "Billy"
+print("We want to know if you like programming!")
+print()
+print("Do you like programming?")
+answer = input()
+print("Great! You said " + answer + "!")
+print("Let's learn some Python today.")
